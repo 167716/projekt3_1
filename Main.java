95773d6f2507d5315055aadbc8c190fea8953f12
167716 <101186863+167716@users.noreply.github.com>
2022-05-17 10:43:00 +0000

    1

--- Main.java
+++ Main.java
@@ -1,5 +1,20 @@
+import java.io.IOException;
+import java.util.Scanner;
+
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    try {
+      Service1 s = new Service1();
+      s.addStudent(new Student("Krzysztof", 20));
+      s.addStudent(new Student("Janusz", 40));
+
+      var students = s.getStudents();
+      for(Student current : students) {
+        System.out.println(current.ToString());
+      }
+    } catch (IOException e) {
+
+    }
+    
   }
 }
