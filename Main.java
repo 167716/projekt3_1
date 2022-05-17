045588d89f2de70f610fb9623f6262a827ec612d
167716 <101186863+167716@users.noreply.github.com>
2022-05-17 11:27:44 +0000

    4

--- Main.java
+++ Main.java
@@ -3,41 +3,55 @@ import java.util.Scanner;
 
 class Main {
   public static void main(String[] args) {
-    Service1 s = new Service1();
-    
-    Scanner scanner = new Scanne(System.in);
-    boolean shouldContinue = true;
-    while (shouldContinue) {
-      System.out.println("1-Dodaj studenta");
-      System.out.println("0-Wyjście");
-      int userChoice = scanner.nextInt();
-      switch (userChoice) {
-        case 1:
-          Scanner scanner = new Scanner(System.in);
-          System.out.println("Podaj imię");
-          String Imie = scanner.nextLine();
-
-          System.out.println("Podaj wiek");
-          int Wiek = scanner.nextInt();
-
-          Service1 s = new Service1();
-          s.addStudent(new Student(Imie, Wiek));
+    try {
+      Scanner skan = new Scanner(System.in);
+     Service1 s = new Service1();
+      int x=4;
+      String imie;
+      String nazwisko;
+      int wiek;
+      String born;
+      System.out.println("Witaj!");
+      while(x!=0)
+        {
+          System.out.println("Wybierz funkcję: \n"+
+                        "1 - dodanie nowego studenta \n"+
+                        "2 - wyszukanie danego studenta \n"+
+                        "3 - wyjście z programu");
+          x=skan.nextInt();
+          switch(x)
+            {
+                case 1:
+                {
+                  Scanner skan1 = new Scanner(System.in);
+                  System.out.println("Podaj imie: ");
+                  imie=skan1.nextLine();
+                  System.out.println("Podaj nazwisko: ");
+                  nazwisko=skan1.nextLine();
+                  System.out.println("Podaj wiek: ");
+                  wiek=skan.nextInt();
+                  System.out.println("Podaj datę urodzenia: ");
+                  born=skan1.nextLine();
+                 s.addStudent(new Student(imie,nazwisko, wiek, born));
+                }break;
+              case 2:
+                {
+                   var students = s.getStudents();
+      for(Student current : students) {
+        System.out.println(current.ToString());
       }
-    }
+                }break;
+              case 3:
+                {
+                  x=0;
+                }break;
+            }
+            
+        }
+      
+     
+    } catch (IOException e) {
 
-    /*
-     * try {
-     * Service1 s = new Service1();
-     * s.addStudent(new Student("Krzysztof", 20));
-     * s.addStudent(new Student("Janusz", 40));
-     * 
-     * var students = s.getStudents();
-     * for(Student current : students) {
-     * System.out.println(current.ToString());
-     * }
-     * } catch (IOException e) {
-     * 
-     * }
-     */
+    }
   }
 }
